ea8ea7589cb475ff04463078954babb1d4323d35
dr-sahrul <sahrulroma17@gmail.com>
2023-09-26 21:03:43 +0700

    Menambahkan jobdest

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,8 @@ class Contoh1 extends CI_Controller
     public function Index(){
         echo "<h1> Perkenalkan </h1>";
         echo "Nama Saya Sahul Roma Doni";
+
+        echo "Saya pejuang rupiah"
     }
    
 }
